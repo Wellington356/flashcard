0238e83a8854674093cb4b9c307033a683a714eb
Wellington356 <160145583+Wellington356@users.noreply.github.com>
2024-11-12 08:38:22 -0300

    Add files via upload

---
+++ pergunstas.js
@@ -0,0 +1,23 @@
+CriaCartao(
+    'Geografia',
+    'Qual é a capital do Brasil?',
+    'A capital é Brasília'
+)
+
+CriaCartao(
+    'Geograia',
+    'Qual é a capital da França?',
+    'A capital é Paris'
+)
+
+CriaCartao(
+    'Geografia',
+    'Qual a capital da Almanha?',
+    'A capial é Berlim'
+)
+
+CriaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é Hi'(RAI)
+)
